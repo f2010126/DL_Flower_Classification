daf615cf7c73d7eca07c69f6e979af13edd197b3
Dipti S <diptibits2010@gmail.com>
2021-01-15 22:18:11 +0100

    SmallCNN added
    - very simple based on Lenet
    - 500k params
    - 50 epochs, 38% accuracy

--- src/cnn.py
+++ src/cnn.py
@@ -30,3 +30,25 @@ class SampleModel(nn.Module):
         x = self.fc2(x)
 
         return x
+
+
+# python -m src.main --model SmallCNN --epochs 25
+class SmallCNN(nn.Module):
+    def __init__(self, input_shape=(3, 64, 64), num_classes=10):
+        super(SmallCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=6, kernel_size=3, padding=0, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=3, stride=1, padding=0)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(4320, 128) # <--
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
