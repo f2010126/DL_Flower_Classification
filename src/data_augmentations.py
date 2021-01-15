2dabbaab7177024f0ea39bc4e36829588c591714
Dipti S <diptibits2010@gmail.com>
2021-01-15 22:13:15 +0100

    FlowerClassification
    copy of the master branch

---
+++ src/data_augmentations.py
@@ -0,0 +1,13 @@
+from torchvision import datasets, transforms
+import torch.nn as nn
+
+resize_to_64x64 = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
+
+resize_and_colour_jitter = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1),
+    transforms.ToTensor()
+])
