d1b449f196da92559b6f08e09740966f3d63bbaa
Dipti S <diptibits2010@gmail.com>
2021-01-17 11:10:06 +0100

    Resize i/p to 128
    -resize i/p to 128, Data augmentation added
    -function to display image.

--- src/data_augmentations.py
+++ src/data_augmentations.py
@@ -6,6 +6,12 @@ resize_to_64x64 = transforms.Compose([
     transforms.ToTensor()
 ])
 
+resize_to_128x128 = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1),
+    transforms.ToTensor()
+])
+
 resize_and_colour_jitter = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ColorJitter(brightness=0.1, contrast=0.1),
