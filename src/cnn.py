5b1938267faa3dc9fd8bb4bc3f3afb5e13233f4e
Dipti S <diptibits2010@gmail.com>
2021-01-17 22:57:08 +0100

    Pre-trained CNN
    - Added a pre-trained CNN vgg, not freezing the weights yet.
    - Added appropriate data transform
    - Linting

--- src/cnn.py
+++ src/cnn.py
@@ -2,6 +2,7 @@
 
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
 
 class SampleModel(nn.Module):
@@ -10,8 +11,8 @@ class SampleModel(nn.Module):
     """
     def __init__(self, input_shape=(3, 64, 64), num_classes=10):
         super(SampleModel, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=10, kernel_size=(3,3), padding=(1,1))
-        self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3,3), padding=(1,1))
+        self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=10, kernel_size=(3, 3), padding=(1, 1))
+        self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(3, 3), padding=(1, 1))
         self.pool = nn.MaxPool2d(3, stride=2)
         # The input features for the linear layer depends on the size of the input to the convolutional layer
         # So if you resize your image in data augmentations, you'll have to tweak this too.
@@ -40,7 +41,7 @@ class SmallCNN(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, stride=1, padding=0)
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=3, stride=1, padding=0)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(4320, 128) # <--
+        self.fc1 = nn.Linear(4320, 128)  # <--
         self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
@@ -53,6 +54,7 @@ class SmallCNN(nn.Module):
         x = self.fc2(x)
         return x
 
+
 # python -m src.main --model SmallCNN2 --epochs 25
 class SmallCNN3(nn.Module):
     def __init__(self, input_shape=(3, 128, 128), num_classes=10):
@@ -61,7 +63,7 @@ class SmallCNN3(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=3, stride=1, padding=0)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(3000, 128) # <--
+        self.fc1 = nn.Linear(3000, 128)  # <--
         self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
@@ -79,19 +81,37 @@ class SmallCNN3(nn.Module):
 class SmallCNN4(nn.Module):
     def __init__(self, input_shape=(3, 128, 128), num_classes=10):
         super(SmallCNN4, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=6, kernel_size=7, padding=0, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0)
+        self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=6, kernel_size=3, padding=0, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=20, kernel_size=3, stride=2, padding=0)
+        self.conv3 = nn.Conv2d(in_channels=20, out_channels=120, kernel_size=3, stride=1, padding=0)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(512, 128) # <--
+        self.fc1 = nn.Linear(3000, 128)  # <--
         self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv3(x))
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
         return x
+
+
+class PreTrainedCNN(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(PreTrainedCNN, self).__init__()
+        self.vggnet = models.vgg16(pretrained=True)
+        num_ftrs = self.vggnet.classifier[6].in_features
+        self.vggnet.classifier[6] = nn.Linear(num_ftrs, num_classes)
+
+    # just in case
+    def set_parameter_requires_grad(self, model, feature_extracting):
+        if feature_extracting:
+            for param in model.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        return self.vggnet(x)
--- src/data_augmentations.py
+++ src/data_augmentations.py
@@ -28,3 +28,14 @@ custom_augment = transforms.Compose([
     # add a normalise here
 ])
 
+pretrain_augmentations = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(p=0.6),
+    transforms.ColorJitter(brightness=0.5, contrast=0.1),
+    transforms.RandomRotation(degrees=45),
+    transforms.RandomVerticalFlip(p=0.3),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
